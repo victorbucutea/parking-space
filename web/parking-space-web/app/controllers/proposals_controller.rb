94de5bafa88634c4f6e3d18140988f11e8e27562
victor.bucutea@gmail.com <victor.bucutea@gmail.com>
2014-08-20 17:04:30 +0100

    added proposals & parking_space functionality

--- web/parking-space-web/app/controllers/proposals_controller.rb
+++ web/parking-space-web/app/controllers/proposals_controller.rb
@@ -9,6 +9,35 @@ class ProposalsController < ApplicationController
     @proposals = Proposal.where parking_space_id: params[:parking_space_id]
   end
 
+  def reject
+
+    @proposal = Proposal.find(params[:proposal_id])
+    owner_deviceid = params[:reject][:owner_deviceid]
+
+    respond_to do |format|
+      if @proposal.reject(owner_deviceid)
+        format.json { render :show, status: :ok, location: parking_space_proposal_url(@proposal.parking_space_id, @proposal) }
+      else
+        format.json { render json: @proposal.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  def approve
+    @proposal = Proposal.find(params[:proposal_id])
+    owner_deviceid = params[:approve][:owner_deviceid]
+
+    respond_to do |format|
+      if @proposal.approve(owner_deviceid)
+        format.json { render :show, status: :ok, location: parking_space_proposal_url(@proposal.parking_space_id, @proposal) }
+      else
+        format.json { render json: @proposal.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
   # POST /parking_spaces/:p_sp_id/proposals
   # POST /parking_spaces/:p_sp_id/proposals.json
   def create
@@ -16,18 +45,37 @@ class ProposalsController < ApplicationController
 
     respond_to do |format|
       if @proposal.save
-        format.json { render :show, status: :created, location: parking_space_proposal_url(@proposal) }
+        format.json { render :show, status: :created, location: parking_space_proposal_url(@proposal.parking_space_id, @proposal) }
       else
         format.json { render json: @proposal.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  # PATCH/PUT /parking_spaces/:p_sp_id/proposals
+  # PATCH/PUT /parking_spaces/:p_sp_id/proposals.json
+  def update
+    #TODO there must be a cleaner way to validate a missing incoming param
+    unless proposal_params['deviceid']
+      render json: {:Error => "Device id is required"}, status: :unprocessable_entity
+      return
+    end
+
+    @proposal = Proposal.find(params[:id])
+
+    respond_to do |format|
+      if @proposal.update(proposal_params)
+        format.json { render :show, status: :ok, location: parking_space_proposal_url(@proposal.parking_space_id, @proposal) }
+      else
+        format.json { render json: @proposal.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def proposal_params
-    params.require(:proposal).permit(:deviceid, :title_message, :bid_amount, :bid_currency, :win_flag)
+    params.require(:proposal).permit(:deviceid, :title_message, :bid_amount, :bid_currency, :bidder_name, :status, :parking_space_id)
   end
 end
--- web/parking-space-web/app/models/proposal.rb
+++ web/parking-space-web/app/models/proposal.rb
@@ -1,6 +1,34 @@
-class Proposal < ActiveRecord::Base
-
+class Proposal < DeviceRecord
+  enum status: [ :pending, :rejected, :approved]
   belongs_to :parking_space
   has_many :messages
 
+  validates :title_message, :presence => true
+  validates :bidder_name, :presence => true
+  validates :status, :presence => true
+  validates :bid_amount, :presence => true
+  validates :bid_currency, :presence => true
+
+  def approve(owner_deviceid)
+
+    if parking_space.deviceid == owner_deviceid
+      approved!
+    else
+      errors.add :deviceid, 'invalid'
+    end
+
+
+    return save
+  end
+
+  def reject(owner_deviceid)
+
+    if parking_space.deviceid == owner_deviceid
+      rejected!
+    else
+       errors.add :deviceid, 'invalid'
+    end
+
+    return save
+  end
 end
