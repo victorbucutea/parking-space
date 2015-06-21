d3cd8ffc3aa77b0b7b5dadbb68101e706991fe9a
Victor Bucutea <victor.bucutea@gmail.com>
2015-06-21 17:53:02 +0300

    notification infrastructure

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,3 +1,6 @@
+require 'pushmeup'
+
+
 class ProposalsController < ApplicationController
   before_action :authenticate_user!
 
@@ -45,6 +48,7 @@ class ProposalsController < ApplicationController
 
     respond_to do |format|
       if @proposal.save
+        notify_space_owner @proposal
         format.json { render :show, status: :created }
       else
         format.json { render json: {Error: @proposal.errors}, status: :unprocessable_entity }
@@ -76,5 +80,22 @@ class ProposalsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def proposal_params
     params.require(:proposal).permit(:deviceid, :phone_number, :title_message, :bid_amount, :bid_currency, :bidder_name, :approval_status, :parking_space_id)
+  end
+
+  private
+
+  def notify_space_owner (proposal)
+    owner_device_id = proposal.parking_space.deviceid
+    owner_space = ParkingSpace.find_by_deviceid owner_device_id
+
+    GCM.host = Rails.application.config.android_gcm_url
+    GCM.format = :json
+    GCM.key = Rails.application.secrets.google_api_key
+
+    destination = [owner_space.notif_registration_id]
+
+    data = {:offer_placed => '1', :msgcnt => "1"}
+
+    GCM.send_notification( destination, data)
   end
 end
