3d1c985943139673fa7876bb9aa95be091849208
victor <victor.bucutea@gmail.com>
2017-03-10 16:46:47 +0200

    tried to fix remove column

--- db/migrate/20140826173756_fix_approved_proposal_id_in_parking_spaces.rb
+++ db/migrate/20140826173756_fix_approved_proposal_id_in_parking_spaces.rb
@@ -1,5 +1,6 @@
 class FixApprovedProposalIdInParkingSpaces < ActiveRecord::Migration
   def change
-    change_column :parking_spaces, :approved_proposal_id, :numeric
+    remove_column :parking_spaces, :approved_proposal_id
+    add_column   :parking_spaces, :approved_proposal_id, :numeric
   end
 end
