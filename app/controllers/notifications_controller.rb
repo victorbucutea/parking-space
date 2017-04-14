69ec56e5f8c4dc062cab4617c6c2ae05ef61182d
victor <victor.bucutea@gmail.com>
2017-04-14 17:00:15 +0300

    fixed notifications, minification and some ui bugs

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,15 @@ class NotificationsController < ApplicationController
   def notif
     if params[:notif_registration_id]
       session[:notif_registration_id] = params[:notif_registration_id]
-      render json: {status: "OK"}, status: :created
+      if current_user == nil
+        return
+      end
+      current_user.notif_registration_id = params[:notif_registration_id]
+      if current_user.save
+        render json: {status: "OK"}, status: :created
+      else
+        render json: {Error: {general: "Registration ID"}}, status: :unprocessable_entity
+      end
     else
       render json: {Error: {general: "Registration ID"}}, status: :unprocessable_entity
     end
