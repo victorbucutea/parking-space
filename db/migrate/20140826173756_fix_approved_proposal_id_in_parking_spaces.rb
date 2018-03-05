da644bf193aa45c8ab3296b99bcd6935caf5b8f8
victor <victor.bucutea@gmail.com>
2018-03-05 22:59:22 +0200

    fixed

--- db/migrate/20140826173756_fix_approved_proposal_id_in_parking_spaces.rb
+++ db/migrate/20140826173756_fix_approved_proposal_id_in_parking_spaces.rb
@@ -1,5 +1,5 @@
 class FixApprovedProposalIdInParkingSpaces < ActiveRecord::Migration[5.1]
   def change
-    change_column :parking_spaces, :approved_proposal_id, :long
+    change_column :parking_spaces, :approved_proposal_id, :integer
   end
 end
