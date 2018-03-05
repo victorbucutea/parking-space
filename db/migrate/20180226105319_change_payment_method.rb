b6b12bcd0e8ef675eda24b292913d2d86ccb5597
victor <victor.bucutea@gmail.com>
2018-03-05 23:21:48 +0200

    fix

--- db/migrate/20180226105319_change_payment_method.rb
+++ db/migrate/20180226105319_change_payment_method.rb
@@ -1,6 +1,6 @@
 class ChangePaymentMethod < ActiveRecord::Migration[5.1]
   def change
-    change_column :proposals, :payment_method, :date
+    change_column :proposals, :payment_method, 'date USING cast(payment_method as date)' 
     rename_column :proposals, :payment_method, :payment_date
   end
 end
