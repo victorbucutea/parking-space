71da7ead92a27104dbf206da6ccb4eab4543fca9
victor <victor.bucutea@gmail.com>
2018-03-05 23:12:41 +0200

    fix

--- db/migrate/20180211152813_change_column_type.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnType < ActiveRecord::Migration[5.1]
-  def change
-    change_column :parking_spaces, :approved_proposal_id, :integer
-    change_column :proposals, :bid_amount, :decimal
-  end
-end
