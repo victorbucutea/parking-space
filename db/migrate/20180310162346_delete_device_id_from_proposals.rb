27636a0a4d507ab84dc271be513ff5868792fca5
victor <victor.bucutea@gmail.com>
2018-03-10 21:05:15 +0200

    type in device id column name

--- db/migrate/20180310162346_delete_device_id_from_proposals.rb
+++ db/migrate/20180310162346_delete_device_id_from_proposals.rb
@@ -2,6 +2,6 @@ class DeleteDeviceIdFromProposals < ActiveRecord::Migration[5.1]
   def change
     remove_column :proposals, :deviceid
     remove_column :parking_spaces, :deviceid
-    remove_column :users, :deviceid
+    remove_column :users, :device_id
   end
 end
