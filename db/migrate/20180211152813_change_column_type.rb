ae83d37a2b44b8a847f55d5a11efc90762552d6a
victor <victor.bucutea@gmail.com>
2018-03-10 20:58:49 +0200

    removed approved_prop_id from pspaces

--- db/migrate/20180211152813_change_column_type.rb
+++ db/migrate/20180211152813_change_column_type.rb
@@ -1,6 +1,6 @@
 class ChangeColumnType < ActiveRecord::Migration[5.1]
   def change
-    change_column :parking_spaces, :approved_proposal_id, :integer
+    remove_column :parking_spaces, :approved_proposal_id
     change_column :proposals, :bid_amount, :decimal
   end
 end
