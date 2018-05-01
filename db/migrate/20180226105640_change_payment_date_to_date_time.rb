0efdffa110ece9a4bfce23abd3a69b873aeb3480
victor <victor.bucutea@gmail.com>
2018-05-01 10:13:47 +0300

    fix migrate

--- db/migrate/20180226105640_change_payment_date_to_date_time.rb
+++ db/migrate/20180226105640_change_payment_date_to_date_time.rb
@@ -1,5 +1,6 @@
 class ChangePaymentDateToDateTime < ActiveRecord::Migration[5.1]
   def change
-    change_column :proposals, :payment_date, :datetime
+    remove_column :proposals, :payment_date
+    add_column :proposals, :payment_date, :datetime
   end
 end
