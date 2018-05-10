b4ba2a7f1ec8c4627a4abfb0a9998b4a98e1b831
victor <victor.bucutea@gmail.com>
2018-05-10 16:36:01 +0300

    fixed service worker size

--- app/assets/service-workers/sw.js
+++ app/assets/service-workers/sw.js
@@ -1,37 +1,32 @@
+
+self.skipWaiting();
+
+
 self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-core.prod.js');
 self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-routing.prod.js');
 self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-strategies.prod.js');
 
-self.addEventListener("fetch", function(event) {
-    console.log(' fetch event triggered');
-});
 
 workbox.routing.registerRoute(
-    new RegExp('.*\.js[^o][^n]'),
+    /.*\.js[^o]/,
     workbox.strategies.staleWhileRevalidate({
         // Use a custom cache name
         cacheName: 'js-cache',
     })
 );
 
+
 workbox.routing.registerRoute(
-    new RegExp('.*/image/.*/logo.*png$'),
+    /.*!\/image\/.*!\/logo.*png$/g,
     workbox.strategies.staleWhileRevalidate({
         // Use a custom cache name
         cacheName: 'img-cache',
     })
 );
 
-workbox.routing.registerRoute(
-    new RegExp('.*/.woff2'),
-    workbox.strategies.staleWhileRevalidate({
-        // Use a custom cache name
-        cacheName: 'img-cache',
-    })
-);
 
 workbox.routing.registerRoute(
-    new RegExp('.*\.css'),
+    /.*\.css/g,
     workbox.strategies.staleWhileRevalidate({
         // Use a custom cache name
         cacheName: 'css-cache',
@@ -39,12 +34,13 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.registerRoute(
-    new RegExp('.*\.html'),
+    /.*\.html/,
     workbox.strategies.staleWhileRevalidate({
         // Use a custom cache name
         cacheName: 'html-cache',
     })
 );
+
 /*
 self.addEventListener('message', function (ev) {
     if (ev.data.action === 'skipWaiting') {
