6181466f925ffe26fec92eecffdb9c46efa01be3
victor <victor.bucutea@gmail.com>
2018-05-10 17:25:29 +0300

    prompt user to install on searching

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -32,7 +32,7 @@ angular.module('ParkingSpaceMobile', [
             console.log('prompting to install !');
             e.preventDefault();
             // Stash the event so it can be triggered later.
-            window.installPrompt = deferredPrompt;
+            window.installPrompt = e;
         });
 
     })
