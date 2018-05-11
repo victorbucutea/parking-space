d89489d246f95d87a34c40d3b479e175d2af9bc6
victor <victor.bucutea@gmail.com>
2018-05-11 18:23:03 +0300

    prompt user to install

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -35,6 +35,30 @@ angular.module('ParkingSpaceMobile', [
             window.installPrompt = e;
         });
 
+
+        Notification.requestPermission(function(status) {
+            console.log('Notification permission status:', status);
+        });
+
+        if (Notification.permission === 'granted') {
+            navigator.serviceWorker.getRegistration().then(function(reg) {
+                var options = {
+                    body: 'Here is a notification body!',
+                    icon: 'images/example.png',
+                    vibrate: [100, 50, 100],
+                    data: {
+                        dateOfArrival: Date.now(),
+                        primaryKey: 1
+                    },
+                    actions: [
+                        {action: 'explore', title: 'Explore this new world',
+                            icon: 'images/checkmark.png'}
+                    ]
+                };
+                reg.showNotification('Hello world!', options);
+            })
+        }
+
     })
 
     .run(['$http','$rootScope', function ($http, $rootScope) {
--- app/assets/service-workers/sw.js
+++ app/assets/service-workers/sw.js
@@ -6,6 +6,9 @@ self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/wo
 self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-routing.prod.js');
 self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-strategies.prod.js');
 
+self.importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js');
+self.importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
+
 
 workbox.routing.registerRoute(
     /.*\.js[^o]?/,
@@ -48,3 +51,38 @@ self.addEventListener('message', function (ev) {
     }
 });*/
 
+
+
+
+firebase.initializeApp({
+    'messagingSenderId': '1036383532323'
+});
+
+var messaging = firebase.messaging();
+
+// If you would like to customize notifications that are received in the background
+// (Web app is closed or not in browser focus) then you should implement this optional method
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    var notificationTitle = 'Background Message Title';
+    var notificationOptions = {
+        body: 'Background Message body.'
+    };
+
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', function(e) {
+    var notification = e.notification;
+    var primaryKey = notification.data.primaryKey;
+    var action = e.action;
+
+    if (action === 'close') {
+        notification.close();
+    } else {
+        clients.openWindow('https://go-park-staging.herokuapp.com');
+        notification.close();
+    }
+});
