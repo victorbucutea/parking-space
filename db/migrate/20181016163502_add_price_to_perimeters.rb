c7d8b52c572f6d8f4b62104c3af055ada1474ace
victor <victor.bucutea@gmail.com>
2018-10-28 23:12:14 +0200

    fix migration

--- db/migrate/20181016163502_add_price_to_perimeters.rb
+++ db/migrate/20181016163502_add_price_to_perimeters.rb
@@ -1,5 +1,5 @@
 class AddPriceToPerimeters < ActiveRecord::Migration[5.1]
   def change
-    add_column :parking_perimeters, :price, :double
+    add_column :parking_perimeters, :price, :decimal
   end
 end
